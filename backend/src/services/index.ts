aa2056e53ab40f8420f13b7f107fc126912a9f50
Mehul Kiran Chaudhari <55375534+MehulKChaudhari@users.noreply.github.com>
2025-06-12 03:02:03 +0530

    first commit

---
+++ backend/src/services/index.ts
@@ -0,0 +1,17 @@
+backend
+├── src
+│   ├── routes
+│   │   └── index.ts
+│   ├── controllers
+│   │   └── index.ts
+│   ├── services
+│   │   └── index.ts
+│   ├── Models
+│   │   └── index.ts
+│   ├── utils
+│   │   └── index.ts
+│   └── app.ts
+├── .prettierrc.json
+├── package.json
+├── tsconfig.json
+└── README.md
