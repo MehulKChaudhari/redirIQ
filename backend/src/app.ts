9ea4275c23c0669dfeccf5406fb7da68f139170e
Mehul Kiran Chaudhari <55375534+MehulKChaudhari@users.noreply.github.com>
2025-06-28 04:25:59 +0530

    add: cors config

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,6 +1,8 @@
 import express, { Express } from 'express';
 import helmet from 'helmet';
 import morgan from 'morgan';
+import cors from 'cors';
+import config from 'config';
 import { errorHandler } from './middleware/errorHandler';
 import { notFoundHandler } from './middleware/notFoundHandler';
 import urlRoutes from './routes';
@@ -8,6 +10,7 @@ import urlRoutes from './routes';
 const app: Express = express();
 
 app.use(helmet());
+app.use(cors(config.get('corsOptions')));
 app.use(morgan('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
--- backend/src/config/production.ts
+++ backend/src/config/production.ts
@@ -1,6 +1,13 @@
 export default {
   nodeEnv: 'production',
 
+  corsOptions: {
+    origin: ['https://rediriq.mehul.wiki'],
+    credentials: true,
+    maxAge: 86400,
+    optionsSuccessStatus: 200,
+  },
+
   database: {
     url: process.env.DATABASE_URL,
   },
