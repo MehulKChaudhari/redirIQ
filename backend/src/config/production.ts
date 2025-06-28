d8c3b874436feb49d93da41e813c89c23661c7b2
Mehul Kiran Chaudhari <55375534+MehulKChaudhari@users.noreply.github.com>
2025-06-29 00:44:42 +0530

    fix: cors

--- backend/src/config/production.ts
+++ backend/src/config/production.ts
@@ -3,8 +3,10 @@ export default {
 
   corsOptions: {
     origin: 'https://rediriq.mehul.wiki',
-    credentials: true,
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type'],
     preflightContinue: false,
+    optionsSuccessStatus: 204,
   },
 
   database: {
