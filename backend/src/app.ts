cb0b74eff19520accaa96d4f4874e9a400256e5d
Mehul Kiran Chaudhari <55375534+MehulKChaudhari@users.noreply.github.com>
2025-06-29 01:04:44 +0530

    remocve helmet and morgan

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,6 +1,4 @@
 import express, { Express } from 'express';
-import helmet from 'helmet';
-import morgan from 'morgan';
 import config from 'config';
 import { errorHandler } from './middleware/errorHandler';
 import { notFoundHandler } from './middleware/notFoundHandler';
@@ -8,8 +6,6 @@ import urlRoutes from './routes';
 
 const app: Express = express();
 
-app.use(helmet());
-app.use(morgan('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
